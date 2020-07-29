14fe1901305d5a63c428c78ada4a7695998d669e
rahul <rahulrajsingh666@gmail.com>
2020-07-29 22:46:59 +0530

    new project creation

---
+++ index.js
@@ -0,0 +1,11 @@
+var contrl = require('./control');
+let a = "Prashant";
+let b = "Singh";
+let c = "Rahul";
+let d = "Singh";
+
+
+var result = contrl.add(a,b);
+//console.log("Addition of two number is:" + ontrl.add(a,b)) 
+console.log("Addition of two number is:" + result) 
+
